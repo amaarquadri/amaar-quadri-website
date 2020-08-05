561dcc3850dc632f0b395d453f7baca9c06e5d8c
Amaar Quadri <amaarquadri@gmail.com>
2020-08-05 02:55:24 -0400

    added more robust url parameter cleaning

--- website/frontend/views.py
+++ website/frontend/views.py
@@ -21,8 +21,20 @@ def play(request):
         # any weird values, or the absence of a value will result in False
         'log-stats': request.GET.get('log-stats', False) == 'true'
     }
+    return render(request, 'frontend/play.html', {'urlParametersJSON': json.dumps(clean_url_parameters(urlParameters))})
 
-    # TODO: handle weird values, number format exception
+
+def clean_url_parameters(urlParameters):
+    for param in ['ai-time', 'ai-positions', 'starting-time', 'increment']:
+        if urlParameters[param] is not None:
+            try:
+                urlParameters[param] = int(urlParameters[param])
+                # every param must be positive except for increment which must be non-negative
+                if urlParameters[param] < 0 if param == 'increment' else urlParameters[param] <= 0:
+                    urlParameters[param] = None
+            except ValueError:
+                urlParameters[param] = None
+
     if urlParameters['ai-time'] is not None:
         urlParameters['ai-positions'] = None
         urlParameters['starting-time'] = None
@@ -44,4 +56,4 @@ def play(request):
     else:
         urlParameters['ai-time'] = 10
 
-    return render(request, 'frontend/play.html', {'urlParametersJSON': json.dumps(urlParameters)})
+    return urlParameters
