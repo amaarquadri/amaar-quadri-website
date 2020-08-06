c550783d3d48de84b41f41c4ac7aae8c6275074e
Amaar Quadri <amaarquadri@gmail.com>
2020-08-06 02:42:38 -0400

    added TODO

--- website/frontend/src/components/play/square.jsx
+++ website/frontend/src/components/play/square.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 export default class Square extends Component {
+    // TODO: preload all possible images at start to prevent mid-game loading
     static PREFIX = '/static/'
 
     render() {
