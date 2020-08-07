9fe6c86e80f12cc62cc3993c385040b15d7deb59
Amaar Quadri <amaarquadri@gmail.com>
2020-08-07 04:32:57 -0400

    removed remnants of react router bs removed unused npm dependancies

--- website/frontend/src/games.jsx
+++ website/frontend/src/games.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "./styles/custom.scss"
 import {render} from "react-dom";
-import {BrowserRouter} from "react-router-dom";
 import GamesPage from "./components/games/games_page.jsx";
 
 render(
     <React.StrictMode>
-        <BrowserRouter>
-            <GamesPage/>
-        </BrowserRouter>
+        <GamesPage/>
     </React.StrictMode>,
     document.getElementById("app"));
