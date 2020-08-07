e14546f5a2920b0f3e633b3836cdfad47a000c28
Amaar Quadri <amaarquadri@gmail.com>
2020-08-07 04:25:19 -0400

    fix weird redirects by abandoning react routers and just using a single link of javascript (bruuuuuuuuuuuh)

--- website/frontend/src/components/games/play_button.jsx
+++ website/frontend/src/components/games/play_button.jsx
@@ -1,14 +1,11 @@
 import React from "react"
 import "../../styles/custom.scss"
-import {useHistory} from "react-router-dom"
 
 
 export default function PlayButton({getLink}) {
-    const history = useHistory();
-
     return (
-        <button type="button" onClick={() => history.push(getLink())}>
+        <button type="button" onClick={() => window.location.href = getLink()}>
             Play Game
         </button>
-    );
+    )
 }
