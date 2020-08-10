87f64caf5ba22a33d59e70e60fa6cf2f2273df07
Amaar Quadri <amaarquadri@gmail.com>
2020-08-10 01:25:21 +0100

    updated allowed host to amaarquadri.com

--- website/website/settings.py
+++ website/website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['www.amaar-quadri.com']
+ALLOWED_HOSTS = ['www.amaarquadri.com']
 
 
 # Application definition
