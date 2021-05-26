6d027ec7146441399185f0ea4dcec1dc96e4f6cf
Amaar Quadri <amaarquadri@gmail.com>
2021-05-26 17:38:49 -0500

    extract content into state, clean up visuals

--- website/frontend/src/components/index/contact.jsx
+++ website/frontend/src/components/index/contact.jsx
@@ -2,6 +2,13 @@ import React, {Component} from "react"
 import "../../styles/custom.scss"
 
 export default class Contact extends Component {
+    state = {
+        text: "Want to get in touch? Give me a call " +
+            "or send me an email and I will get back to you as soon as possible!",
+        phoneNumber: "+1 (647) 767-1796",
+        email: "amaarquadri@gmail.com"
+    }
+
     render() {
         return (
             <section className="page-section" id="contact">
@@ -10,20 +17,20 @@ export default class Contact extends Component {
                         <div className="col-lg-8 text-center">
                             <h2 className="mt-0">Let's Get In Touch!</h2>
                             <hr className="divider my-4"/>
-                            <p className="text-muted mb-5">Ready to start your next project with us? Give us a call or
-                                send us an email and we will get back to you as soon as possible!</p>
+                            <p className="text-muted mb-5">{this.state.text}</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                             <i className="fas fa-phone fa-3x mb-3 text-muted"/>
-                            <div>+1 (647) 767-1796</div>
+                            <div>{this.state.phoneNumber}</div>
                         </div>
                         <div className="col-lg-4 mr-auto text-center">
                             <i className="fas fa-envelope fa-3x mb-3 text-muted"/>
-                            <a className="d-block" href="mailto:contact@amaarquadri.com">contact@amaarquadri.com</a>
+                            <a className="d-block" href={"mailto:" + this.state.email}>{this.state.email}</a>
                         </div>
                     </div>
+                    <hr className="divider my-4"/>
                 </div>
             </section>
         )
