143183b56631a9f631789c69f98718ebfe05769f
Amaar Quadri <amaarquadri@gmail.com>
2021-05-26 17:28:51 -0500

    improve visuals of about section

--- website/frontend/src/components/index/about.jsx
+++ website/frontend/src/components/index/about.jsx
@@ -10,11 +10,11 @@ export default class About extends Component {
                         <div className="col-lg-8 text-center">
                             <h2 className="text-white mt-0">Your next intern!</h2>
                             <hr className="divider light my-4"/>
-                            <p className="text-white-50 mb-4">I'm a Mechanical Engineering student at the University of
+                            <p className="text-black-50 mb-4"><b>I'm a Mechanical Engineering student at the University of
                                 Waterloo. I'm experienced with SolidWorks (CSWE), Finite Element Analysis, and GD&T. I
                                 also have extensive programming experience in various languages including Python, Java,
                                 and C++. I have a passion for using all of my skills for designing mechanical devices
-                                from the ground up! </p>
+                                from the ground up!</b></p>
                             <a className="btn btn-light btn-xl js-scroll-trigger"
                                href={staticURL + "Mechanical_Engineering_Resume.pdf"}>Resume for Mechanical Positions</a>
                             &nbsp;&nbsp;&nbsp;
